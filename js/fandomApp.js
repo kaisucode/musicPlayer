ba1247e1e6f8149cf170d2a6cc4610f43f86b94d
Shadowsinger <kevin.hsukaihao@gmail.com>
2018-08-25 05:47:29 +0800

    Fandom slideshow done

--- js/fandomApp.js
+++ js/fandomApp.js
@@ -1,7 +1,44 @@
 
+function fandomAppSleepCommand(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function fandomAppSleep() {
+  await fandomAppSleepCommand(3000);
+	fandomApp.appInitialization2();
+}
+
 fandomApp = {
-	appInitialization(){
-		$("#fandom_container").html("<img src='data/fandom/life motto.jpeg'/>");
+	
+
+	slideIndex : 0,
+	numOfImgs : 0,
+	fandomImages : [],
+
+	loadLibrary(){
+		$.getJSON( "data/fandomImgList.json", function( data ) {
+			$.each( data, function( key, val ) {
+				fandomApp.fandomImages.push(val);
+				fandomApp.numOfImgs++;
+			});
+		});
 	},
+
+	appInitialization(){
+		this.loadLibrary();
+		$("#fandom_container").html("<img src='data/fandom/life motto.jpeg'/>");
+		fandomAppSleep();
+	},
+	appInitialization2(){
+		console.log(this.fandomImages);
+		this.showSlides();
+	},
+
+	showSlides() {
+    if (fandomApp.slideIndex >= fandomApp.numOfImgs-1)
+			fandomApp.slideIndex = 0;
+    $("#fandom_container").html("<img src='data/fandom/"+fandomApp.fandomImages[fandomApp.slideIndex]+"'/>");
+    fandomApp.slideIndex++;
+    setTimeout(fandomApp.showSlides, 3000); // Change image every 2 seconds
+	}
 }
 
