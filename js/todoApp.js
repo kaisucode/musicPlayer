6e776f97779133a05eedd795dba53aeca34a253b
Shadowsinger <kevin.hsukaihao@gmail.com>
2018-08-24 21:23:57 +0800

    Trying to work out animation of hw (todo)

--- js/todoApp.js
+++ js/todoApp.js
@@ -9,16 +9,41 @@ var todoApp = {
 					allText = rawFile.responseText;
 		}
 		rawFile.send(null);
-		console.log(allText);
+		// console.log(allText);
 		return allText;
 	},
 	
 	appInitialization(){
-		byId('hw_content').innerText = this.readTextFile("data/todo/hw.txt");
+		// byId('hw_content').innerText = this.readTextFile("data/todo/hw.txt");
 		byId('cs_content').innerText = this.readTextFile("data/todo/cs.txt");
 		byId('ee_content').innerText = this.readTextFile("data/todo/ee.txt");
 		byId('art_content').innerText = this.readTextFile("data/todo/art.txt");
-	}	
 
+		var hw_content = this.readTextFile("data/todo/hw.txt");
+		var parsed_hw_content = hw_content.split("\n");
+		console.log(parsed_hw_content);
+		var final_hw_content = [];
+		var tempStr = "";
+		parsed_hw_content.forEach(function(line){
+			if (line == ""){
+				tempStr+="\n";
+				final_hw_content.push(tempStr);
+				tempStr = "";
+			}
+			else{
+				tempStr+=(line+"\n");
+			}
+		});
+		console.log(final_hw_content);
+
+		byId('hw_content').innerText = final_hw_content[0];
+		var i = 1;
+		var hw_content_length = final_hw_content.length;
+		setInterval(function() {
+			byId('hw_content').innerText = final_hw_content[i];
+			(i >= hw_content_length) ? i=0 : i++;
+		}, 2000);
+
+	}	
 }
 
